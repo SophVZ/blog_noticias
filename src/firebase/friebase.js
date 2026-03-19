db00efa9e1087c7224b82ac60c4cdff450fe2b75
Sofia Valenzuela Zuccar <sv.zuccar@gmail.com>
2026-03-19 14:27:46 -0300

    config firebase

---
+++ src/firebase/friebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore'
+
+export const firebaseConfig = {
+    apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+    authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.VUE_APP_FIREBASE_APP_ID
+};
+
+export const firebaseApp = initializeApp(firebaseConfig);
+export const auth=getAuth(firebaseApp);
+export const db=getFirestore(firebaseApp);
